6eb44fa5cc307f9e9e794ebb7c3251e00383763e
liruiping <245852750@qq.com>
2017-06-30 14:16:14 +0800

    新建新闻列表文件

---
+++ TaekwondoAssociation/Main/HomePage/ViewController/NewsListViewController.m
@@ -0,0 +1,50 @@
+//
+//  NewsListViewController.m
+//  TaekwondoAssociation
+//
+//  Created by 李瑞平 on 2017/6/30.
+//  Copyright © 2017年 Miss 李. All rights reserved.
+//
+
+#import "NewsListViewController.h"
+
+@interface NewsListViewController ()
+
+@end
+
+@implementation NewsListViewController
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.title = @"新闻";
+    }
+    return self;
+}
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    [self loadWebUrl:@"http://g3msy5.natappfree.cc/h5/news/newsList.html"];
+}
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self.navigationController setNavigationBarHidden:NO animated:NO];
+    self.tabBarController.tabBar.hidden = NO;
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
