0277e61564923577963441061b62afb447acf46a
王齐 <2449974535@qq.com>
2017-06-30 15:36:41 -0700

    收藏

---
+++ TaekwondoAssociation/Main/MemberCenter/View/MyCollectionViews/TANoPicCollectionCell.m
@@ -0,0 +1,24 @@
+//
+//  TANoPicCollectionCell.m
+//  TaekwondoAssociation
+//
+//  Created by 伟宏 on 2017/6/30.
+//  Copyright © 2017年 Miss 李. All rights reserved.
+//
+
+#import "TANoPicCollectionCell.h"
+
+@implementation TANoPicCollectionCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
