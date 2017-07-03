32f528250530ee2457724351544c6f249b9347ce
王尉坤 <wwk.4325261@163.com>
2017-07-03 11:04:35 +0800

    添加认证页面

---
+++ TaekwondoAssociation/Main/HomePage/ViewController/CertificationViewController.m
@@ -0,0 +1,43 @@
+//
+//  CertificationViewController.m
+//  TaekwondoAssociation
+//
+//  Created by 王尉坤 on 2017/7/3.
+//  Copyright © 2017年 Miss 李. All rights reserved.
+//
+
+#import "CertificationViewController.h"
+
+@interface CertificationViewController ()
+
+@end
+
+@implementation CertificationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (IBAction)inquireBtnCLick:(id)sender {
+}
+
+- (IBAction)helpBtnClick:(id)sender {
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
