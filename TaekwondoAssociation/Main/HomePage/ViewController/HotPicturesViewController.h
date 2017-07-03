4d3dc8df5d2f3a55e007bc0703fb925719558464
liruiping <245852750@qq.com>
2017-07-03 16:36:44 +0800

    新建热门图库  名人堂更多文件

---
+++ TaekwondoAssociation/Main/HomePage/ViewController/HotPicturesViewController.h
@@ -0,0 +1,13 @@
+//
+//  HotPicturesViewController.h
+//  TaekwondoAssociation
+//
+//  Created by 李瑞平 on 2017/7/3.
+//  Copyright © 2017年 Miss 李. All rights reserved.
+//
+
+#import "BaseWebViewController.h"
+
+@interface HotPicturesViewController : BaseWebViewController
+
+@end
